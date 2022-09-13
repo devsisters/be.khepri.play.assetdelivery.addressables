b56a03f80bb2344a869f1ccaeb23b86eedca6cb9
Jungeun.Jin <41898398+jungeunjin-dev@users.noreply.github.com>
2022-09-13 10:05:56 +0900

    Update Debug.cs
    실제 사용하는 mars-client와 맞추어 Development빌드에서만 출력되게 Define수정

--- Runtime/Debug.cs
+++ Runtime/Debug.cs
@@ -4,7 +4,7 @@ namespace Khepri.AssetDelivery
 {
     public class Debug
     {
-        [Conditional("DEBUG_PLAYASSETDELIVERY")]
+        [Conditional("DEBUG")]
         public static void LogFormat(string format, params object[] args)
         {
             UnityEngine.Debug.LogFormat(format, args);
